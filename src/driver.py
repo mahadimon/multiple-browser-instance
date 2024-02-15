2fa5fd17ab651a2d46fe9ec21d208277b4b8837c
unknown <mahadimon101@gmail.com>
2024-02-15 23:16:15 +0600

    add cashier login and logout support

--- src/driver.py
+++ src/driver.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from configuration import Configuration
 from util import GetActivationKey
 import asyncio
+from configuration import PosUser
+
 class Driver:
     def __init__(self, profilePath, configuration):
         self.profile = webdriver.ChromeOptions()
@@ -12,12 +14,14 @@ class Driver:
         self.driver = webdriver.Chrome(options=self.profile)
         self.configuration: Configuration = configuration
 
-    async def RunBrowser(self, posnumber):
+    async def RunBrowser(self, posuser):
         self.driver.get(self.configuration.url)
-        await self.WelcomeLogin(posnumber)
+        await self.WelcomeLogin(posuser)
         await self.PressConnect()
+        await self.CashierLogin(posuser)
+        await self.CashierLogOut()
 
-    async def WelcomeLogin(self, posnumber):
+    async def WelcomeLogin(self, posuser):
         input_username_field = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//input[@id='userName']")))
         input_username_field.send_keys(self.configuration.username)
 
@@ -25,7 +29,7 @@ class Driver:
         input_password_field.send_keys(self.configuration.password)
 
         input_activation_key_field = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//input[@id='activationKey']")))
-        input_activation_key_field.send_keys(GetActivationKey(self.configuration.chainnumber, self.configuration.storenumber, posnumber))
+        input_activation_key_field.send_keys(GetActivationKey(self.configuration.chainnumber, self.configuration.storenumber, posuser.posnumber))
 
         press_connect_button = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "submit-button")))
         press_connect_button.click()
@@ -33,9 +37,37 @@ class Driver:
         await asyncio.sleep(20)
 
     async def PressConnect(self):
-        press_connect = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//login-lock/button/span[1]")))
-        press_connect.click()
-        await asyncio.sleep(5)
+        try:
+            press_connect = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//login-lock/button/span[1]")))
+            press_connect.click()
+            await asyncio.sleep(5)
+        except Exception as ex:
+            print(f"Unexpected: {ex}, {type(ex)=}")
 
+    async def CashierLogin(self, posuser: PosUser):
+        try:
+            input_cashier_number = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//input[@id='cashierIdInput']")))
+            input_cashier_number.send_keys(posuser.cashiernumber)
 
+            input_cashier_number = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//input[@id='cashierPasswordInput']")))
+            input_cashier_number.send_keys(posuser.cashierpassword)
+
+            press_continue_button = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//fiftytwo-login/login-fields/button")))
+            press_continue_button.click()
+
+            await asyncio.sleep(20)
+        except Exception as ex:
+            print(f"Unexpected: {ex}, {type(ex)=}")
+
+    async def CashierLogOut(self):
+        try:
+            press_menu_button = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//fiftytwo-icon/mat-icon[contains(text(), 'menu')]")))
+            press_menu_button.click()
+            await asyncio.sleep(10)
+            press_sign_off_button = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//mat-icon[contains(text(), 'exit_to_app')]")))
+            press_sign_off_button.click()
+
+            await asyncio.sleep(10)
+        except Exception as ex:
+            print(f"Unexpected: {ex}, {type(ex)=}")
     
--- src/main.py
+++ src/main.py
@@ -3,19 +3,20 @@ import os, shutil, json
 from util import GetProfilePath
 from driver import Driver
 import asyncio
-from configuration import Configuration
+from configuration import Configuration, PosUser
 
 settings=""
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
-async def ClearProfile(pos):
-    profilePath = GetProfilePath(pos)
-    if os.path.exists(profilePath):
-        shutil.rmtree(profilePath)
+async def ClearProfile(posusers: list[PosUser]):
+    for posuser in posusers:
+        profilePath = GetProfilePath(posuser.posnumber)
+        if os.path.exists(profilePath):
+            shutil.rmtree(profilePath)
 
-async def CreateProfile(posnumbers):
-    for pos in posnumbers:
-        profilePath = GetProfilePath(pos)
+async def CreateProfile(posusers: list[PosUser]):
+    for posuser in posusers:
+        profilePath = GetProfilePath(posuser.posnumber)
         if os.path.exists(profilePath):
             shutil.rmtree(profilePath)
         os.makedirs(profilePath, exist_ok=True)
@@ -26,16 +27,19 @@ async def main():
         settings = json.load(file)
     
     configuration = Configuration(**settings)
-    await CreateProfile(configuration.posnumbers)
+    #print(configuration.posusers)
+    configuration.posusers = [PosUser(**data) for data in configuration.posusers]
+    #print(configuration.posusers)
+    await CreateProfile(configuration.posusers)
 
-    for pos in configuration.posnumbers:
-        driver: Driver = Driver(GetProfilePath(pos), configuration)
-        task = asyncio.create_task(driver.RunBrowser(pos))
+    for posuser in configuration.posusers:
+        driver: Driver = Driver(GetProfilePath(posuser.posnumber), configuration)
+        task = asyncio.create_task(driver.RunBrowser(posuser))
         async_tasks.append(task)
     
     asyncio.wait(await asyncio.gather(*async_tasks), timeout=None, return_when=asyncio.ALL_COMPLETED)
     
-    await ClearProfile(configuration.posnumbers)
+    #await ClearProfile(configuration.posusers)
 
 if __name__== "__main__":
     asyncio.run(main())
